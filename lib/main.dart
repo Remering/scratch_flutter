8408ca4eb9c92fdd6c4e473ba7361ffaac870d0b
Remering <1015488424@qq.com>
2020-05-18 09:50:27 +0800

    add slash screen and make MainScaffold the main scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,118 +1,51 @@
+import 'package:auto_orientation/auto_orientation.dart';
 import 'package:flutter/material.dart';
-import 'package:scratch_flutter/widget/top_bar.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:logging/logging.dart';
+import 'package:scratch_flutter/bloc/account/account_event.dart';
+import 'package:scratch_flutter/bloc/providers.dart';
+import 'package:scratch_flutter/constants.dart';
+import 'package:scratch_flutter/ui/widget/main_scaffold.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() => runApp(MyApp());
+import 'bloc/account/account_bloc.dart';
+import 'bloc/api_service/profile_api_service.dart';
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+void main() {
+  Logger.root.level = Level.ALL;
+  Logger.root.onRecord.listen((record) {
+    print("[${record.time}]${record.level}: ${record.message}");
+  });
+
+  runApp(MultiBlocProvider(
+    child: App(),
+    providers: providers,
+  ));
+  AutoOrientation.portraitAutoMode();
+}
+
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    const whiteTextStyle = TextStyle(
-      color: Colors.white,
-    );
-    const whiteTextTheme = TextTheme(
-      title: whiteTextStyle,
-      button: whiteTextStyle,
-      subhead: whiteTextStyle,
-      caption: whiteTextStyle,
-      overline: whiteTextStyle,
-    );
-    const whiteIconThemeData = IconThemeData(
-      color: Colors.white
-    );
+    ThemeData theme = Theme.of(context);
+    theme = theme.copyWith(primaryColor: Colors.orange);
 
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primaryColor: Colors.orange,
-        accentColor: Colors.amber,
-        primaryTextTheme: whiteTextTheme,
-        accentTextTheme: whiteTextTheme,
-//        primaryIconTheme: whiteIconThemeData,
-        accentIconTheme: whiteIconThemeData,
+      home: FutureBuilder<SharedPreferences>(
+        future: SharedPreferences.getInstance(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            final token = snapshot.data.getString(JWT_KEY);
+            if (token == null) return MainScaffold();
+            final accountBloc = BlocProvider.of<AccountBloc>(context);
+            final profileApiBloc = BlocProvider.of<ProfileApiBloc>(context);
+            accountBloc.dispatch(SetToken(token, profileApiBloc.currentState));
+          }
+          return MainScaffold();
+        },
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      theme: theme,
     );
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: TopBar(),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
 }
