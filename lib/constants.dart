a1faa2ce3f127cfb80b5a4207d79856ae7f3b6ec
Remering <1015488424@qq.com>
2020-05-18 09:49:33 +0800

    move all global variables to constants.dart

---
+++ lib/constants.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+const int SUCCESS = 0;
+const int INFO = 1;
+const int WARNING = 2;
+const int ERROR = 3;
+
+const STUDENT = 0;
+const TEACHER = 1;
+
+const COLORS = [Colors.green, Colors.blue, Colors.yellow, Colors.red];
+
+const BASE_URL = "http://192.168.10.100:8888";
+
+const JWT_KEY = "scratch-jwt";
